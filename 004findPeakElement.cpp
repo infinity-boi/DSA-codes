7b995b1108fc6bfaa003da8f5f47ac6eb539ff9f
infinity-boi <gouravsinghc2003@gmail.com>
2024-01-26 23:11:01 +0530

    Binary Search

---
+++ 004findPeakElement.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// 162 leetcode
+
+// A peak element is an element that is strictly greater than its neighbors.
+
+// Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
+
+// You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
+
+// You must write an algorithm that runs in O(log n) time.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: 2
+// Explanation: 3 is a peak element and your function should return the index number 2.
+
+int findPeakElement(vector<int> &nums)
+{
+    int low = 0, high = nums.size() - 1;
+    if (nums.size() == 1)
+        return 0;
+    if (nums.size() == 0)
+        return -1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (mid > 0 && mid < nums.size() - 1)
+        {
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+            {
+                return mid;
+            }
+            else if (nums[mid] > nums[mid - 1] &&
+                     nums[mid] < nums[mid + 1])
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        else if (mid == 0)
+        {
+            if (nums[mid] > nums[mid + 1])
+                return mid;
+            else
+            {
+                return mid + 1;
+            }
+        }
+        else if (mid == nums.size() - 1)
+        {
+            if (nums[mid] > nums[mid - 1])
+                return mid;
+            else
+            {
+                return mid - 1;
+            }
+        }
+    }
+    return 0;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums;
+    while (n--)
+    {
+        int x;
+        cin >> x;
+        nums.push_back(x);
+    }
+    int ans = findPeakElement(nums);
+    cout << ans;
+    return 0;
+}
