e537da1753d8df9ea1e37e4e6334db814e5241d4
infinity-boi <gouravsinghc2003@gmail.com>
2024-01-27 01:18:55 +0530

    Recursion

--- 003powerUsingRecursion.cpp
+++ 003powerUsingRecursion.cpp
@@ -3,19 +3,19 @@
 #include <iostream>
 using namespace std;
 
-int square(int a, int n){
+int power(int a, int n){
     if(n==0) return 1;
     if(n%2==0){
-        return square(a, n/2) * square(a, n/2);
+        return power(a, n/2) * power(a, n/2);
     }
     else{
-        return square(a, n/2) * square(a, n/2) * a;
+        return power(a, n/2) * power(a, n/2) * a;
     }
 }
 
 int main(){
     int n, a;
     cin>> a >>n;
-    cout<< square(a, n);
+    cout<< power(a, n);
     return 0;
 }
