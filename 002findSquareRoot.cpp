38100f73bb576d965f78ffee0e3a00739f08f23a
infinity-boi <gouravsinghc2003@gmail.com>
2024-01-27 00:33:52 +0530

    Binary Search

--- 002findSquareRoot.cpp
+++ 002findSquareRoot.cpp
@@ -1,9 +1,11 @@
+// Binary Search
+
 #include <iostream>
 using namespace std;
 
 // Find the square root of x without builtin function
 // Binary Search : Works for finding sqrt(n) but not sqrt(x), where x is a number. It only gives integer values of sqrt(n).
-float squareRoot(int n){
+float squareRoot(int n, int p){
     int s = 0;
     int e = n;
     int ans;
@@ -17,13 +19,25 @@ float squareRoot(int n){
             s = mid+1;
         }
     }
+    float fans = ans;
+    float inc = 0.1;
+    for(int t= 1; t<=p; t++){
+        while(fans*fans<=n){
+            fans+= inc;
+        }
+        fans -= inc;
+        inc = inc/ 10;
+    }
+    return fans;
 }
 
 int main(){
     int n; 
     cin>> n;
+    int p;
+    cin>> p;
     // Binary search method to find sqrt(n)
-    float ans = squareRoot(n);
+    float ans = squareRoot(n, p);
     cout<< ans<< endl;
     return 0;
 }
