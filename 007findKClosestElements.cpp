47ff44bfb2b686e9350d5d24365297a9f250822c
infinity-boi <gouravsinghc2003@gmail.com>
2024-07-22 18:48:29 +0530

    Binary Search

--- 007findKClosestElements.cpp
+++ 007findKClosestElements.cpp
@@ -25,16 +25,16 @@ using namespace std;
 // Output: [1,2,3,4]
 
 vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int left = 0, right = arr.size()-k;
-        while(left< right){
-            int mid = (left +right) /2;
-            if(x- arr[mid] > arr[mid+k] - x)  left = mid+1;
-            else {
-                right = mid;
-            }
+    int left = 0, right = arr.size()-k;
+    while(left< right){
+        int mid = (left +right) /2;
+        if(x- arr[mid] > arr[mid+k] - x)  left = mid+1;
+        else {
+            right = mid;
         }
-        return vector<int>(arr.begin() + left, arr.begin()+left+k);
     }
+    return vector<int>(arr.begin() + left, arr.begin()+left+k);
+}
 
 int main(){
     int n;
@@ -43,7 +43,7 @@ int main(){
     while(n--){
         int in;
         cin>> in;
-        arr.push_back(x);
+        arr.push_back(in);
     }
     int k;
     cin>> k;
